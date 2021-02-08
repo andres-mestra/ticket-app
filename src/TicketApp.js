1c67347e1ec9132545cacc2a06f479ef03232784
andresdev <andresmestra11a@gmail.com>
2021-02-08 16:22:38 -0500

    Router diseño

---
+++ src/TicketApp.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { TicketRouter } from './router/TicketRouter';
+
+function TicketApp() {
+  return (
+    <>
+      <TicketRouter />
+    </>
+  );
+}
+
+export default TicketApp;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
+import TicketApp from './TicketApp';
 
 ReactDOM.render(
-  <App />,
+  <TicketApp />,
   document.getElementById('root')
 );
