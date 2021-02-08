bc35bce6317d283ab16f8dd3fefc961f963c0103
andresdev <andresmestra11a@gmail.com>
2021-02-08 17:30:17 -0500

    Estructura pagina de escritorio

--- src/pages/DesktopPage.js
+++ src/pages/DesktopPage.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import { Button, Col, Divider, Row, Typography } from 'antd'
+import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons'
+
+const { Title, Text } = Typography
 
 export const DesktopPage = () => {
+
+  const handleSalir = () => {
+
+  }
+
+  const handleNextTicket = () => {
+    
+  }
+  
+
+
   return (
-    <div>
-      Descktop page
-    </div>
+    <>
+      <Row>
+        <Col span={20}>
+          <Title level={2} >Andres</Title>
+          <Text>Usted esta trabajando en el escritorio: </Text>
+          <Text type="success" style={{ fontSize: "1.2em" }} >5</Text>
+        </Col>
+
+        <Col span={4} align="right" >
+          <Button
+            shape="round"
+            type="danger"
+            onClick={handleSalir}
+          >
+            <CloseCircleOutlined />
+            Salir
+          </Button>
+        </Col>
+      </Row>
+
+      <Divider />
+
+      <Row>
+        <Col>
+          <Text>Esta atendiendo el ticket numero:  </Text>
+          <Text
+            style={{ fontSize: '1.5em' }}
+            type="danger"
+          >55</Text>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col offset={18} span={6} align="right">
+          <Button
+            onClick={handleNextTicket}
+            shape="round"
+            type="primary"
+          >
+            Siguiente
+            <RightOutlined />
+          </Button>
+        </Col>
+      </Row>
+    </>
   )
 }
