27beb4f12c230276ff318f6689d45663f7be08ce
andresdev <andresmestra11a@gmail.com>
2021-02-08 16:41:18 -0500

    Rutas

--- src/router/TicketRouter.js
+++ src/router/TicketRouter.js
@@ -6,38 +6,65 @@ import {
   UploadOutlined,
 } from '@ant-design/icons';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import { EntryPage } from '../pages/EntryPage';
+import { ColaPage } from '../pages/ColaPage';
+import { CreateTicketPage } from '../pages/CreateTicketPage';
+import { DesktopPage } from '../pages/DesktopPage';
+
 const { Sider, Content } = Layout;
 
 export const TicketRouter = () => {
   return (
-    <Layout style={{height: '100vh'}}>
-      <Sider>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
+    <Router>
+      <Layout style={{ height: '100vh' }}>
+        <Sider hidden={false}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              <Link to="/ingresar">
+                Ingresar
+              </Link>
             </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              <Link to="/cola">
+                Cola de tickects
+              </Link>
             </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              <Link to="/crear">
+                Crear Ticket
+              </Link>
             </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-       
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          Content
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Switch>
+              <Route path="/ingresar" component={ EntryPage } />
+              <Route path="/cola" component={ ColaPage } />
+              <Route path="/crear" component={ CreateTicketPage } />
+
+              <Route path="/escritorio" component={ DesktopPage } />
+              <Redirect to="/ingresar" />
+            </Switch>
           </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   )
 }
